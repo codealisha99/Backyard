a0b9d200b37f2a6219eb932c817063401b12c6f1
codealisha99 <codealisha99@gmail.com>
2026-04-08 23:44:20 +0530

    adding react frontend

--- websoctfront/frontend/src/App.tsx
+++ websoctfront/frontend/src/App.tsx
@@ -1,41 +1,50 @@
-import { useState , useEffect  } from 'react'
 import './App.css'
 
 
-
-
-
-function App() {
-    const [wss, setWs] = useState();
-    const [client, setClient] = useState();
-
-  function sendMessage(){
-    if (wss) {
-      wss.send("Hello from the client!"); //this is the sending of message to the server and we will send it to where it is thus providing it the link
-    }
-  };
-
-  useEffect(() => {
-    const wss = new WebSocket('ws://localhost:8080');
-     //this is the creation of connection and we will connect to where it is thus providing it the link
-    setWs(wss);
-     wss.onmessage = (e) => {
-      console.log(e.data);
-     }
-     const client = new WebSocket("ws://localhost:8081");
-     setClient(wss1);
-     ws1.onmessage = (event) => {
-      console.log(event.data);
-     }
-
-  }, []);
-
- return (
+function chat(){
+  return (
     <div>
-     <input  type="text" placeholder='Message...'></input>
-     <button onClick={sendMessage}>SEND</button>
+     hi there
     </div>
   )
+
 }
 
-export default App
+
+export default chat
+
+
+// function App() {
+//     const [wss, setWs] = useState();
+//     const [client, setClient] = useState();
+
+//   function sendMessage(){
+//     if (wss) {
+//       wss.send("Hello from the client!"); //this is the sending of message to the server and we will send it to where it is thus providing it the link
+//     }
+//   };
+
+//   useEffect(() => {
+//     const wss = new WebSocket('ws://localhost:8080');
+//      //this is the creation of connection and we will connect to where it is thus providing it the link
+//     setWs(wss);
+//      wss.onmessage = (e) => {
+//       console.log(e.data);
+//      }
+//      const client = new WebSocket("ws://localhost:8081");
+//      setClient(wss1);
+//      ws1.onmessage = (event) => {
+//       console.log(event.data);
+//      }
+
+//   }, []);
+
+//  return (
+//     <div>
+//      <input  type="text" placeholder='Message...'></input>
+//      <button onClick={sendMessage}>SEND</button>
+//     </div>
+//   )
+// }
+
+// export default App
