b03f4824c6da9a65ebfc00a6d11cae130d08971e
codealisha99 <codealisha99@gmail.com>
2026-04-14 23:06:02 +0530

    feat: setup mong project with mongoose and env configuration

---
+++ mong/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const user = new Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const todo = new Schema({
+    title: String,
+    userID: ObjectId,
+    completed: Boolean
+});
+
+const UserModel = mongoose.model('Users', user);
+const TodoModel = mongoose.model('Todos', todo);
+
+module.exports = {
+    UserModel: UserModel,
+    TodoModel: TodoModel
+}
